adf57aec2ea076c32871a77b00424764d0b08549
Alpha-Anas <muhd.anas70pe@gmail.com>
2023-12-18 21:42:22 +0500

    nothing changed

--- Coin.cpp
+++ Coin.cpp
@@ -3,7 +3,7 @@
 
 Coin::Coin(int x, int y, SDL_Texture* c_tex):GameObject(x,y,c_tex){}
 
-// int Coin::coinCounter = 0;
+int Coin::coinCounter = 0;
 
 void Coin::increaseCoinCounter() {
     coinCounter++;
@@ -16,38 +16,3 @@ int Coin::GetCoinCounter() {
 void Coin::ResetCoinCounter() {
     coinCounter = 0;
 }
-WaterCoin::WaterCoin(int x, int y, SDL_Texture* c_tex):Coin(x,y,c_tex){}
-
-// int WaterCoin::coinCounter = 0;
-
-void WaterCoin::increaseCoinCounter() {
-    coinCounter++;
-}
-
-int WaterCoin::GetCoinCounter() {
-    return coinCounter;
-}
-
-void WaterCoin::ResetCoinCounter() {
-    coinCounter = 0;
-}
-
-
-FireCoin::FireCoin(int x, int y, SDL_Texture* c_tex): Coin(x,y,c_tex){}
-
-// int WaterCoin::coinCounter = 0;
-
-void FireCoin::increaseCoinCounter() {
-    coinCounter++;
-}
-
-int FireCoin::GetCoinCounter() {
-    return coinCounter;
-}
-
-void FireCoin::ResetCoinCounter() {
-    coinCounter = 0;
-}
-
-
-
